3e4fba125344cbb3ff28d95854c3faed471a9fbc
alexabadp <aalex.abad96@gmail.com>
2023-04-19 22:58:49 -0500

    estilos globales

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
 
 function css(done) {
   src("src/scss/**/*.scss") //identificar  el archivo SASS
+    .pipe(plumber())
     .pipe(sass()) //Compilarlo
     .pipe(dest("build/css")); //Almacenarlo en el disco duro
 
